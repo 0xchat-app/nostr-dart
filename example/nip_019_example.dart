8f3c9506f578519f0cd0b46e98db027912a97512
0xchatTeam <130329555+0xchatTeam@users.noreply.github.com>
2023-04-25 16:55:29 +0800

    add nip1

--- example/nip_019_example.dart
+++ example/nip_019_example.dart
@@ -4,7 +4,7 @@ void main() {
   var sender = Keychain.generate();
   print(sender.public);
   String pubKeyHex = sender.public;
-  final bech32PubKey = Nip19.encode('npub', pubKeyHex);
+  final bech32PubKey = Nip19.encode('npub', "3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d");
   print('Bech32 encoded public key: $bech32PubKey');
 
   final decodedData = Nip19.decode(bech32PubKey);
---
+++ lib/src/nips/nip_001.dart
@@ -0,0 +1,15 @@
+import 'package:nostr/nostr.dart';
+
+class Nip1 {
+  static Event setMetadata(String content, String privkey) {
+    return Event.from(kind: 0, tags: [], content: content, privkey: privkey);
+  }
+
+  static Event textNote(String content, String privkey) {
+    return Event.from(kind: 1, tags: [], content: content, privkey: privkey);
+  }
+
+  static Event recommendServer(String content, String privkey) {
+    return Event.from(kind: 2, tags: [], content: content, privkey: privkey);
+  }
+}
