33fc38c3f7f9a3068db90fe66bfcd1ead1dcdf8b
0xchatTeam <130329555+0xchatTeam@users.noreply.github.com>
2023-05-09 15:44:20 +0800

    add nip010

---
+++ lib/src/nips/nip_010.dart
@@ -0,0 +1,45 @@
+import 'package:nostr/nostr.dart';
+
+class Nip10 {
+  static Thread decode(Event event){
+    List<ETags> etags = [];
+    List<PTags> ptags = [];
+    for (var tag in event.tags) {
+      if (tag[0] == "p") ptags.add(PTags(tag[1], tag[2]));
+      if (tag[0] == "e") etags.add(ETags(tag[1], tag[2], tag[3]));
+    }
+    return Thread(etags, ptags);
+  }
+
+  static List<List<String>> toTags(List<ETags> etags, List<PTags> ptags) {
+    List<List<String>> result = [];
+    for (var etag in etags) {
+      result.add(["e", etag.eventId, etag.relayURL, etag.marker]);
+    }
+    for (var ptag in ptags) {
+      result.add(["e", ptag.pubkey, ptag.relayURL]);
+    }
+    return result;
+  }
+}
+
+class ETags {
+  String eventId;
+  String relayURL;
+  String marker; // root/reply/mention
+
+  ETags(this.eventId, this.relayURL, this.marker);
+}
+
+class PTags {
+  String pubkey;
+  String relayURL;
+
+  PTags(this.pubkey, this.relayURL);
+}
+
+class Thread {
+  List<ETags> etags;
+  List<PTags> ptags;
+  Thread(this.etags, this.ptags);
+}
