dc1754bc770aa7fb3ea2b5ba4952348019cf2f76
water783 <wdashi783@gmail.com>
2023-06-12 21:43:50 +0800

    add ProfileMention

--- lib/nostr.dart
+++ lib/nostr.dart
@@ -18,6 +18,8 @@ export 'src/nips/nip_009.dart';
 export 'src/nips/nip_010.dart';
 export 'src/nips/nip_019.dart';
 export 'src/nips/nip_020.dart';
+export 'src/nips/nip_021.dart';
+export 'src/nips/nip_027.dart';
 export 'src/nips/nip_028.dart';
 export 'src/nips/nip_033.dart';
 export 'src/nips/nip_051.dart';
