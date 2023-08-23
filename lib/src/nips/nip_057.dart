8d0e523cdd742c709db73baa348c486ad84d4ba1
water783 <wkuangji@outlook.com>
2023-08-23 18:16:54 +0800

    update ZapReceipt

--- lib/src/nips/nip_057.dart
+++ lib/src/nips/nip_057.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:nostr_core_dart/nostr.dart';
 
 class Nip57 {
   static ZapReceipt getZapReceipt(Event event) {
     if (event.kind == 9735 && event.content.isEmpty) {
-      String? bolt11, preimage, description, recipient, eventId;
+      String? bolt11, preimage, description, recipient, eventId, content, sender;
       for (var tag in event.tags) {
         if (tag[0] == 'bolt11') bolt11 = tag[1];
         if (tag[0] == 'preimage') preimage = tag[1];
@@ -11,8 +13,19 @@ class Nip57 {
         if (tag[0] == 'p') recipient = tag[1];
         if (tag[0] == 'e') eventId = tag[1];
       }
+      if(description != null){
+        try{
+          Map map = jsonDecode(description);
+          content = map['content'];
+          sender = map['pubkey'];
+        }
+        catch(_){
+          content = '';
+        }
+      }
+
       ZapReceipt zapReceipt = ZapReceipt(event.createdAt, event.pubkey, bolt11!,
-          preimage!, description!, recipient!, eventId);
+          preimage!, description!, recipient!, eventId, content, sender);
       return zapReceipt;
     } else {
       throw Exception("${event.kind} is not nip57 compatible");
@@ -43,13 +56,15 @@ class Nip57 {
 
 class ZapReceipt {
   int paidAt;
-  String pubkey;
+  String zapper;
   String bolt11;
   String preimage;
   String description;
   String recipient;
   String? eventId;
+  String? content;
+  String? sender;
 
-  ZapReceipt(this.paidAt, this.pubkey, this.bolt11, this.preimage,
-      this.description, this.recipient, this.eventId);
+  ZapReceipt(this.paidAt, this.zapper, this.bolt11, this.preimage,
+      this.description, this.recipient, this.eventId, this.content, this.sender);
 }
