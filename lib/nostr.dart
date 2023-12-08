c649c03d5ed595703bda9caa34437f73b4efc9be
water783 <wkuangji@outlook.com>
2023-12-08 16:21:43 +0800

    add nip23

--- lib/nostr.dart
+++ lib/nostr.dart
@@ -21,6 +21,7 @@ export 'src/nips/nip_010.dart';
 export 'src/nips/nip_019.dart';
 export 'src/nips/nip_020.dart';
 export 'src/nips/nip_021.dart';
+export 'src/nips/nip_023.dart';
 export 'src/nips/nip_024.dart';
 export 'src/nips/nip_027.dart';
 export 'src/nips/nip_028.dart';
