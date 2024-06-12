4ef90ac6b3662f02a67bd1b1a00d4d5c51fead0a
water783 <wkuangji@outlook.com>
2024-06-12 16:46:49 +0800

    add authString

--- lib/src/nips/nip_042.dart
+++ lib/src/nips/nip_042.dart
@@ -5,9 +5,9 @@ import 'package:nostr_core_dart/nostr.dart';
 ///https://github.com/nostr-protocol/nips/blob/master/42.md
 ///Authentication of clients to relays
 class Nip42 {
-  static Future<String> encode(
+  static Future<Event> encode(
       String challenge, String relay, String pubkey, String privkey) async {
-    Event event = await Event.from(
+    return await Event.from(
         kind: 22242,
         tags: [
           ["relay", relay],
@@ -16,15 +16,18 @@ class Nip42 {
         content: "",
         pubkey: pubkey,
         privkey: privkey);
+  }
+
+  static String authString(Event event) {
     var auth = ["AUTH", event.toJson()];
     return jsonEncode(auth);
   }
 
-  static bool authRequired(String message){
+  static bool authRequired(String message) {
     return message.startsWith('auth-required: ');
   }
 
-  static bool restricted(String message){
+  static bool restricted(String message) {
     return message.startsWith('restricted: ');
   }
 }
