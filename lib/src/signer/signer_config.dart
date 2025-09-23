dd4da73e90d3ee464dc77cc3306edfcd0afe9fd2
W <130329555+wcat7@users.noreply.github.com>
2025-09-24 00:28:05 +0800

    Update signer_config.dart

--- lib/src/signer/signer_config.dart
+++ lib/src/signer/signer_config.dart
@@ -35,7 +35,7 @@ class SignerConfigs {
       packageName: 'com.greenart7c3.nostrsigner',
       displayName: 'Amber',
       iconName: 'icon_login_amber.png',
-      callMethod: SignerCallMethod.contentProvider,
+      callMethod: SignerCallMethod.intent,
       contentProviderUris: {
         'get_public_key': 'content://com.greenart7c3.nostrsigner.GET_PUBLIC_KEY',
         'sign_event': 'content://com.greenart7c3.nostrsigner.SIGN_EVENT',
@@ -67,7 +67,7 @@ class SignerConfigs {
       packageName: 'com.github.haorendashu.nowser',
       displayName: 'Nowser',
       iconName: 'nowser.png',
-      callMethod: SignerCallMethod.contentProvider,
+      callMethod: SignerCallMethod.intent,
       contentProviderUris: {
         'get_public_key': 'content://com.github.haorendashu.nowser.GET_PUBLIC_KEY',
         'sign_event': 'content://com.github.haorendashu.nowser.SIGN_EVENT',
